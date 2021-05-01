a69325cd9fae57ae35a7dd8ab3cb3ca5eaed0a54
MegalLink <alexresidentevil@gmail.com>
2021-05-01 20:43:24 +0000

    Environment prod

--- src/app/services/noticias.service.ts
+++ src/app/services/noticias.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import {environment} from '../../environments/environment'
+import {environment} from '../../environments/environment.prod'
 import { RespuestaTopHeadLines } from '../interfaces/interfaces';
 
 const apiKey=environment.apiKey;
